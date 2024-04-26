b2d855f46f1dd6eb908a0d59118b036d10a71a4f
mhmddefrizal <muhammaddefrizal08@gmail.com>
2024-04-26 19:14:03 +0700

    first commit

---
+++ POPUP BOX pada JAVASCRIPT/main.js
@@ -0,0 +1,21 @@
+// alert("Hello world!");
+// var nama = prompt("Masukkan nama : ");
+// alert("Nama anda : " + nama);
+// var tes = confirm("kamu yakin");
+// if (tes === true) {
+//   alert("User menekan OK !");
+// } else {
+//   alert("User menekan Cancel");
+// }
+
+alert("Selamat Datang");
+var lagi = true;
+
+while (lagi === true) {
+  var nama = prompt("Masukkan nama : ");
+  alert("halo " + nama);
+
+  lagi = confirm("coba lagi?");
+}
+
+alert("terima kasih");
