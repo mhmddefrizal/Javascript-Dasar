9327ddd79a29a1c09d9878ab60a9b7a0b1d12a51
mhmddefrizal <muhammaddefrizal08@gmail.com>
2024-04-28 02:51:26 +0700

    control-flow

---
+++ CONTROL FLOW pada JAVASCRIPT/main.js
@@ -0,0 +1,12 @@
+// alert("Mulai");
+// for (var i = 0; i < 5; i++) {
+//   alert("Hello World");
+// }
+// alert("Selesai");
+
+var angka = prompt("Masukkan angka : ");
+if (angka % 2 === 0) {
+  alert(angka + " adalah bilangan genap");
+} else {
+  alert(angka + " adalah bilangan ganjil");
+}
